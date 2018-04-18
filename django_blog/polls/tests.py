b25e255d92ad52c08188b0edb975864c31c7b22d
deng.yj@maisiteng.com <zaq12wsx>
2018-04-18 22:53:14 +0800

    modify

--- django_blog/polls/tests.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-from django.test import TestCase
-
-# Create your tests here.
--- django_blog/run_gunicorn_port.wsgi
+++
@@ -1,1 +0,0 @@
-gunicorn django_blog.wsgi:application --bind realhu.tk:8001
